5679840ea37ea4fab2fc7e78a847b339172b9842
geksilla <geksilla@gmail.com>
2013-09-09 10:33:31 +0300

    remove break from main loop

--- lib/vk_wall_crawler/wall.rb
+++ lib/vk_wall_crawler/wall.rb
@@ -5,14 +5,12 @@ module VkWallCrawler
     class << self
       def fetch(owner_id, break_limit = 0)
         owner_id = owner_id.to_id if owner_id.index(/http|vk.com/)
-        puts owner_id
         @client = VkontakteApi::Client.new
         @data = []
         count = 100
-        total_pages = (@vk.wall.get(owner_id: owner_id.to_i, count: 1).first.to_f/count).ceil
+        total_pages = (@client.wall.get(owner_id: owner_id.to_i, count: 1).first.to_f/count).ceil
         total_pages.times do |step|
-          break
-          @wall = @client.wall.get(owner_id: owner_id, count: count, offset: step*count)
+          @wall = @client.wall.get(owner_id: owner_id.to_i, count: count, offset: step*count)
           @wall.each_with_index do |post, index|
             next if index == 0
             break if post.date < break_limit

572e6917cfe1135804ac44ea43fe15baec9e4cf9
geksilla <geksilla@gmail.com>
2013-09-11 17:22:05 +0300

    search by word added, faraday config added

--- lib/vk_wall_crawler.rb
+++ lib/vk_wall_crawler.rb
@@ -1,15 +1,14 @@
 require "hashie"
 require "vkontakte_api"
-require "nokogiri"
 
 require "vk_wall_crawler/version"
 require "vk_wall_crawler/adapter"
 require "vk_wall_crawler/helpers/string"
 require "vk_wall_crawler/wall"
 require "vk_wall_crawler/config"
+require "vk_wall_crawler/search"
 
 module VkWallCrawler
   extend VkWallCrawler::Configuration
-
 
 end
--- lib/vk_wall_crawler/version.rb
+++ lib/vk_wall_crawler/version.rb
@@ -1,3 +1,3 @@
 module VkWallCrawler
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
